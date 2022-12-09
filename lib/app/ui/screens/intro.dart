de3bba403b19eb127f3027b89875547314f8f1e7
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-09 20:23:28 +0000

    added media

--- lib/app/ui/screens/intro.dart
+++ lib/app/ui/screens/intro.dart
@@ -7,6 +7,8 @@ class Intro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
           color: Colors.black,
         ),
