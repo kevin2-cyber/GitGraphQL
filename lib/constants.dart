03204771906ebad51121a7ce2692fbba351b17f4
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-21 16:29:18 +0000

    setting up client

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,12 @@ class AppConstants {
   // .env file path
   static const filePath = 'assets/.env';
 
+  // api url
+  static const baseUrl = 'https://api.github.com/graphql';
+
+  // token name
+  static const token = 'PERSONAL_ACCESS_TOKEN';
+
   // font
   static const fontFamily = 'One UI Sans';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gitgraph/app.dart';
+import 'package:gitgraph/constants.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  // a better error screen
+  ErrorWidget.builder = (FlutterErrorDetails details) => Material(
+    color: Colors.greenAccent.shade100,
+    child: Center(
+      child: Text(
+        details.exceptionAsString(),
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+            fontFamily: AppConstants.fontFamily),
+      ),
+    ),
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const GitGraph());
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
+
+  await dotenv.load(fileName: AppConstants.filePath);
+
+  String token = dotenv.get(AppConstants.token);
+
+  await initHiveForFlutter();
+
+  final HttpLink httpLink = HttpLink(
+    AppConstants.baseUrl,
+  );
+
+  final AuthLink authLink = AuthLink(
+    getToken: () async => 'Bearer $token'
+  );
+
+  final Link link = authLink.concat(httpLink);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: link,
+      cache: GraphQLCache(store: HiveStore())
+    ),
+  );
+  runApp(GraphQLProvider(client: client, child: const GitGraph()));
 }
